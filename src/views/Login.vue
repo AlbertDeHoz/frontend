0832c1c8e048496cc2999d01d5d11ab3db85f734
Albert <alberto.entreprises@gmail.com>
2020-12-09 07:56:18 -0500

    Se agrego sweet alert

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TheLogin></TheLogin>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import TheLogin from '@/components/TheLogin.vue'
 
 export default {
-  name: 'Home',
+  name: 'Login',
   components: {
-    HelloWorld
+    TheLogin
   }
 }
 </script>
